d48728c2ba6c47cd441116355e6517f6c36bb99f
Mert18213 <119688513+Mert18213@users.noreply.github.com>
2026-01-13 14:42:51 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "XXXX",
-  authDomain: "XXXX.firebaseapp.com",
-  projectId: "XXXX",
-  storageBucket: "XXXX.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyBTkuXoSn4NpORgbFLltl8qvXD5D00lM1s",
+  authDomain: "koristagarage-ab9fb.firebaseapp.com",
+  projectId: "koristagarage-ab9fb",
+  storageBucket: "koristagarage-ab9fb.firebasestorage.app",
+  messagingSenderId: "580178615656",
+  appId: "1:580178615656:web:f3340c896afff18e624add",
+  measurementId: "G-TZ92WETRX6"
 };
 
 firebase.initializeApp(firebaseConfig);
